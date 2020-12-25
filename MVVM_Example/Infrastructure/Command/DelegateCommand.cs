23784d50f52081c71e2d59cbbd1ab1f9920546da
Vlad Karmazin <37839328+kain69@users.noreply.github.com>
2020-12-26 02:12:04 +0500

    Хм

---
+++ MVVM_Example/Infrastructure/Command/DelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_Example.Infrastructure.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        //Поля
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        //События
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        //Конструктор
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        
+        //Методы
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _execute(parameter);
+    }
+}
---
+++ MVVM_Example/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using MVVM_Example.Infrastructure.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_Example.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        private int _Clicks;
+
+        public int Clicks
+        {
+            get => _Clicks;
+            set => Set(ref _Clicks, value);
+        }
+
+        public ICommand ClickAdd => new DelegateCommand(obj => Clicks++, obj => Clicks < 10);
+    }
+}
